f4ee3ed79848916e9550f42945a98dd221a8a0dd
TanzidAnan <tanzid.anan125@gmail.com>
2024-10-31 21:26:25 +0600

    password fild

--- src/Compontents/StateFullForm/StateFullForm.jsx
+++ src/Compontents/StateFullForm/StateFullForm.jsx
@@ -2,14 +2,20 @@ import { useState } from "react";
 
 const StateFullForm = () => {
     const [email,setEmai] =useState(null)
+    const [password,setPassword] =useState(null)
     const hendleSubmit = e => {
         e.preventDefault();
         console.log(email)
+        console.log(password)
     }
 
     const hendleEmailChange =(e) =>{
         // console.log(e.target.value);
         setEmai(e.target.value)
+
+    }
+    const hendlePasswordchange = e =>{
+        setPassword(e.target.value)
 
     }
     return (
@@ -21,7 +27,9 @@ const StateFullForm = () => {
                 onChange={hendleEmailChange}
                 className='py-3 text-slate-50 mt-2' type="text" name="email" id="" placeholder='Enter Text' />
                 <br />
-                <input className='py-3 text-slate-50 mt-2' type="password" name="password" id="" placeholder='Enter Text' />
+                <input
+                onChange={hendlePasswordchange}
+                className='py-3 text-slate-50 mt-2' type="password" name="password" id="" placeholder='Enter Text' />
                 <br />
                 <input className='bg-orange-500 mt-3 p-2 rounded-md' type="submit" value="submit" />
             </form>
