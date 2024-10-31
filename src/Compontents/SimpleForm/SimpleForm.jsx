108c9bc97177bea9363251011d6e85714213ff2b
TanzidAnan <tanzid.anan125@gmail.com>
2024-10-31 16:47:22 +0600

    data

--- src/Compontents/SimpleForm/SimpleForm.jsx
+++ src/Compontents/SimpleForm/SimpleForm.jsx
@@ -4,12 +4,15 @@ const SimpleForm = () => {
     const hendleSubmit =(e) =>{
         e.preventDefault()
         console.log(e.target.name.value)
+        console.log(e.target.email.value)
         console.log(4545454)
     }
     return (
         <div className='w-6/12 mx-auto '>
             <form className='bg-slate-300 p-32' onSubmit={hendleSubmit}>
             <input className='py-3 text-slate-50' type="text" name="name" id="" placeholder='Enter Text'/>
+            <br />
+            <input className='py-3 text-slate-50 mt-2' type="text" name="email" id="" placeholder='Enter Text'/>
             <br />
             <input className='bg-orange-500 mt-3 p-2 rounded-md' type="submit" value="submit" />
             </form>
