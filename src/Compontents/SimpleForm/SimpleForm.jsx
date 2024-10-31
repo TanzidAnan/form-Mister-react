d112d7288265142bf07fd0eb4c8fc1d8e73699a5
TanzidAnan <tanzid.anan125@gmail.com>
2024-10-31 16:31:19 +0600

    form style

--- src/Compontents/SimpleForm/SimpleForm.jsx
+++ src/Compontents/SimpleForm/SimpleForm.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 
 const SimpleForm = () => {
+    
     return (
-        <div>
-            
+        <div className='w-6/12 mx-auto '>
+            <form className='bg-slate-300 p-32'>
+            <input className='py-3 text-slate-50' type="text" name="" id="" placeholder='Enter Text'/>
+            <br />
+            <input className='bg-orange-500 mt-3 p-2 rounded-md' type="submit" value="submit" />
+            </form>
         </div>
     );
 };
