f3c266eae980ea8ee2c56a603e7e73f37ab90ea4
TanzidAnan <tanzid.anan125@gmail.com>
2024-10-31 16:34:32 +0600

    preventDefault

--- src/Compontents/SimpleForm/SimpleForm.jsx
+++ src/Compontents/SimpleForm/SimpleForm.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 
 const SimpleForm = () => {
-    
+    const hendleSubmit =(e) =>{
+        e.preventDefault()
+        console.log(4545454)
+    }
     return (
         <div className='w-6/12 mx-auto '>
-            <form className='bg-slate-300 p-32'>
+            <form className='bg-slate-300 p-32' onSubmit={hendleSubmit}>
             <input className='py-3 text-slate-50' type="text" name="" id="" placeholder='Enter Text'/>
             <br />
             <input className='bg-orange-500 mt-3 p-2 rounded-md' type="submit" value="submit" />
