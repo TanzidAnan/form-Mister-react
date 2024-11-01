332acf23f53264d3a107767f4dfb988c4df94660
TanzidAnan <tanzid.anan125@gmail.com>
2024-11-01 15:45:51 +0600

    updete

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,7 @@ function App() {
       {/* <RefForm></RefForm> */}
       {/* <HookForm></HookForm> */}
       <ReusableForm formTitle={'sign up'}></ReusableForm>
+      <ReusableForm formTitle={'profile Updete'}></ReusableForm>
     </>
   )
 }
